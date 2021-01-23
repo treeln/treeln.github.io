a51d1842a61dddd21b4841d162e5c0b93ca9a356
Suman Senapati <sigma.6.2009@gmail.com>
2021-01-23 16:49:31 +0530

    Home button executed

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
                     var h = $('#home').height();
                     var a = $('#about').height();
                     var t = $('#tests').height();
+                   
                     homebtn.click(function() {
                         $(window).scrollTop(h);
                     });
@@ -17,7 +18,20 @@
                     $('#testsbtn').click(function() {
                         $(window).scrollTop(3*h);
                     });
-
+                    $(window).scroll(function(){
+                        var position = $(window).scrollTop();
+                        if(position>200)
+                        {
+                            $('#homescrollbtn').show(600);
+                        }
+                        else
+                        {
+                            $('#homescrollbtn').hide(600)
+                        }
+                    });
+                    $('#homescrollbtn').click(function(){
+                        $(window).scrollTop(0);
+                    })
 ////////////////////////////////////////////////////////////
                     // Test tab toggle
 ///////////////////////////////////////////////////////////
@@ -44,13 +58,13 @@
                         $('.tc1').css({'background-color':'#ccc','color':'#000'});
                     });
                         $('#instrct_bttn').click(function(){
-                                $('#instrct').show("slow");
+                                $('#instrct').show(500);
 
 
                         });
 
                         $('#close_inst').click(function() {
-                                $('#instrct').hide(2000);
+                                $('#instrct').hide(500);
                         });
 
 
